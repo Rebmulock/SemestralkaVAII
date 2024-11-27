26ad7ae5b972e959f9bfb91a65e0f4b7086e8651
Rebmulock <david.kolumber150@gmail.com>
2024-11-27 16:57:47 +0100

    Refactor & fix code multiplicity of service cards in scaling.html

---
+++ frontend/javascript/ServiceCardComponent.jsx
@@ -0,0 +1,56 @@
+const ServiceCardComponent = () => {
+    const services = [
+        {
+            title: 'Class: One-To-Many',
+            imgSrc: '../images/people-group-royal-purple.svg',
+            imgAlt: 'group of many people',
+            pros: 'Low cost',
+            cons: 'No time for personal problem-solving',
+            price: 100,
+            buttonText: 'Buy now',
+        },
+        {
+            title: 'Class: One-To-Five',
+            imgSrc: '../images/people-five-royal-purple.svg',
+            imgAlt: 'group of five people',
+            pros: 'More personal approach',
+            cons: 'Each participant may have varying levels of experience and needs',
+            price: 400,
+            buttonText: 'Buy now',
+        },
+        {
+            title: 'Class: One-To-One',
+            imgSrc: '../images/people-single-royal-purple.svg',
+            imgAlt: 'single person',
+            pros: 'Our attention is reserved exclusively for you',
+            cons: 'Limited availability.',
+            price: 500,
+            buttonText: 'Qualify now',
+        },
+    ];
+
+    const serviceCards = services.map(service => (
+        <div className="col-md-4 mb-4">
+            <div className="card h-100 d-flex">
+                <div className="card-body d-flex flex-column">
+                    <p className="main-text">{service.title}</p>
+                    <img className="d-block mx-auto" src={service.imgSrc}
+                         alt={service.imgAlt}/>
+                    <p className="card-text"><strong>Pros: </strong>{service.pros}</p>
+                    <p className="card-text"><strong>Cons: </strong>{service.cons}</p>
+                    <p className="card-text mt-auto"><span><strong>Price:</strong> ${service.price}</span>
+                        <button className="btn btn-primary btn-qualify">{service.buttonText}</button>
+                    </p>
+                </div>
+            </div>
+        </div>
+    ))
+
+    return (
+        <div className="row">
+            {serviceCards}
+        </div>
+    )
+}
+
+export default ServiceCardComponent
--- frontend/javascript/scaling.jsx
+++ frontend/javascript/scaling.jsx
@@ -2,9 +2,40 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../css/navbar.css'
 import NavbarComponent from "./NavbarComponent.jsx";
+import ServiceCardComponent from "./ServiceCardComponent.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <NavbarComponent />
+        <NavbarComponent/>
+
+        <main>
+            <div className="nav-break"></div>
+
+            <p className="main-text">Our Services</p>
+
+            <ServiceCardComponent/>
+
+            <div className="nav-break"></div>
+
+            <p className="main-text">Not satisfied?</p>
+            <p className="main-text">Contact us for tailored services for your needs!</p>
+
+            <form className="contact-form">
+                <div className="mb-3">
+                    <label htmlFor="name" className="main-text-thin">Name</label>
+                    <input type="text" className="form-control" id="name" placeholder="Your Name" required/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="main-text-thin">Email</label>
+                    <input type="email" className="form-control" id="email" placeholder="name@example.com" required/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="message" className="main-text-thin">Message</label>
+                    <textarea className="form-control" id="message" rows="4" placeholder="Your message..."
+                              required></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Send Message</button>
+            </form>
+        </main>
     </StrictMode>,
 )
