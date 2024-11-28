41b6d216e2a65611a1894b1b8664b684bd6ea3e2
Rebmulock <david.kolumber150@gmail.com>
2024-11-28 17:06:26 +0100

    Add React import to component files

--- frontend/javascript/components/FormComponent.jsx
+++ frontend/javascript/components/FormComponent.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const FormComponent = () => {
     const options = [{
             id: "beginner",
--- frontend/javascript/components/NavbarComponent.jsx
+++ frontend/javascript/components/NavbarComponent.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const NavbarComponent = () => {
     return (
         <nav className="navbar">
