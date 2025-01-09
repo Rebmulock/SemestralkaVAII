14c168e1fcc85172ef1ace840fcd23e8858fba9c
Rebmulock <david.kolumber150@gmail.com>
2025-01-09 17:22:03 +0100

    Change image from ImageField to FileField & add extension validator

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,10 +1,13 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.contrib.auth.models import User
 
 class ContentBlock(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to="content/")
+    image = models.FileField(upload_to="content/", null=True, blank=True, validators=[
+        FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])
+    ])
     image_alt = models.CharField(max_length=30)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="content_blocks")
 
