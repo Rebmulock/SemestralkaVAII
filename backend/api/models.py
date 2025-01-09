82e4aada33da6f1f20baf93843ee7c4dff204957
Rebmulock <david.kolumber150@gmail.com>
2025-01-09 16:30:36 +0100

    Fix file field and migrate changes

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,10 +1,14 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.contrib.auth.models import User
 
 class ContentBlock(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to="content/")
+    image_url = models.FileField(upload_to="content/",
+                                 validators=[FileExtensionValidator(["jpg", "jpeg", "png"])],
+                                 null=True,
+                                 blank=True)
     image_alt = models.CharField(max_length=30)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="content_blocks")
 
